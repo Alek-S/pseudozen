10acd77327b0f49f7530f588ebb8bac9261240c3
Alek Shnayder <alek.shnayder@gmail.com>
2017-07-08 11:18:01 -0500

    Initial folder and file setups

---
+++ controller/api_routes.js
@@ -0,0 +1,12 @@
+const request = require('request');
+
+//model
+//TODO
+//ex: const Article = require('../model/Article.js');
+
+
+module.exports = function(app) {
+
+	//===API ROUTES===
+
+};
---
+++ test/test.js
@@ -0,0 +1,43 @@
+const request = require('supertest');
+const server = require('../server');
+const chalk = require('chalk');
+
+//===HTML Routes===
+describe( chalk.yellow('HTML Routes:'), function () {
+	afterEach(function () {
+		server.close();
+	});
+
+	it('responds to GET / with 200', function(done) {
+		request(server)
+			.get('/')
+			.expect(200, done);
+	});
+
+	it('responds to GET /assets/css/style.css with 200', function(done) {
+		request(server)
+			.get('/assets/css/style.css')
+			.expect(200, done);
+	});
+
+	it('responds to GET /bundle.js with 200', function(done) {
+		request(server)
+			.get('/bundle.js')
+			.expect(200, done);
+	});
+
+	it('responds to GET /assets/images/logo.svg with 200', function(done) {
+		request(server)
+			.get('/assets/images/logo.svg')
+			.expect(200, done);
+	});
+});
+
+
+
+//===API Routes===
+describe(chalk.yellow('API Routes:'), function () {
+	afterEach(function () {
+		server.close();
+	});
+});
